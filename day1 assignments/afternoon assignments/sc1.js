9cd1a46402657134bc5b6c949e328bcc11fff238
Manjunath2002 <85491849+Manjunath2002@users.noreply.github.com>
2021-06-08 17:04:44 +0530

    Add files via upload

---
+++ day1 assignments/afternoon assignments/sc1.js
@@ -0,0 +1,9 @@
+
+			function changecolor() {
+				document.getElementById("abs").style.color="green";
+
+			}
+			function blackcolor() {
+				document.getElementById("abs").style.color="black";
+			}
+		
